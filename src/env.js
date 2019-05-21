496a83abf9483a15dd73888d898ccc7c0a8fa773
asmita-kalena <asmita.kalena.23@gmail.com>
2019-05-21 21:11:53 +0530

    Changed url

--- src/env.js
+++ src/env.js
@@ -1,18 +1,21 @@
 // API URLs related to evn
 const env = {
-  development: 'http://localhost:3000',
-  production : 'https://rails-to-do-list-narola.herokuapp.com'
-}
+  development: "https://rails-to-do-list-narola.herokuapp.com",
+  production: "https://rails-to-do-list-narola.herokuapp.com"
+};
 
 // hosts that will use this App
 let hosts = {
- development: ['localhost'],
- production: ['react-to-do-list-narola.herokuapp.com'] 
-}
+  development: ["localhost"],
+  production: ["react-to-do-list-narola.herokuapp.com"]
+};
 
-const hostname = window.location.hostname
+const hostname = window.location.hostname;
 console.log(hostname);
-export const current_env = Object.keys(hosts).filter(host => hosts[host].includes(hostname))
+export const current_env = Object.keys(hosts).filter(host =>
+  hosts[host].includes(hostname)
+);
 console.log(current_env);
-export const API_URL = current_env.length != 0 ? env[current_env.toString()] : env.development
+export const API_URL =
+  current_env.length != 0 ? env[current_env.toString()] : env.development;
 console.log(API_URL);
